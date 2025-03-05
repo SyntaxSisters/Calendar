5c76c0edf82055e707ab4366a8fdff02c397784e
Hayden Dunn <tiavina.dunn@gmail.com>
2025-03-05 13:12:57 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,19 +1,23 @@
 @echo off
 cd /d "%~dp0"
+cd src
 if not exist ".venv\" (
     echo Creating virtual environment...
     python -m venv .venv
     echo Virtual environment created.
     call .venv\Scripts\activate
     echo Installing Flet...
-    python3 -m pip install -r requirements.txt
+    python3 -m pip install -r ../requirements.txt
     echo Installing python-dateutil...
     pip install python-dateutil
+    pip install flet
+    pip install typing_extensions
 ) else (
     call .venv\Scripts\activate
-    python3 -m pip install -r requirements.txt
-    echo Installing python-dateutil...
+    python3 -m pip install -r ../requirements.txt
     pip install python-dateutil
+    pip install flet
+    pip install typing_extensions
 )
 
 python app.py
