46b0e3bb9b33d4aac1a52551f0f95b14f03039e9
Hayden Dunn <tiavina.dunn@gmail.com>
2025-03-05 13:23:57 -0500

    Decluttered

--- main.bat
+++ main.bat
@@ -5,19 +5,13 @@ if not exist ".venv\" (
     echo Creating virtual environment...
     python -m venv .venv
     echo Virtual environment created.
-    call .venv\Scripts\activate
-    echo Installing Flet...
-    python3 -m pip install -r ../requirements.txt
-    echo Installing python-dateutil...
-    pip install python-dateutil
-    pip install flet
-    pip install typing_extensions
+    call .venv\Scripts\activate.bat
+    echo Installing requirements...
+    pip install -r ../requirements.txt
 ) else (
-    call .venv\Scripts\activate
-    python3 -m pip install -r ../requirements.txt
-    pip install python-dateutil
-    pip install flet
-    pip install typing_extensions
+    call .venv\Scripts\activate.bat
+    echo Installing requirements...
+    pip install -r ../requirements.txt
 )
 
 python app.py
