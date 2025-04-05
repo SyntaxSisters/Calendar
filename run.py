80ec074e3f8556e2151df38116163a41549cfb44
HTD006 <hayden.dunn@ontariotechu.net>
2025-04-04 23:11:03 -0400

    Fixed

--- run.py
+++ run.py
@@ -2,14 +2,14 @@ import os
 import sys
 import subprocess
 
-PYTHON_EXE = "python3" if sys.platform != "win32" else "py -3.13"
+PYTHON_EXE = "python3" if sys.platform != "win32" else "py"
 
 venv_dir = ".venv"
 venv_activate = os.path.join(venv_dir, "Scripts", "activate") if sys.platform == "win32" else os.path.join(venv_dir, "bin", "activate")
 
-def run_command(command, shell=True):
+def run_command(command):
     """ Runs a command in the system shell. """
-    subprocess.run(command, shell=shell, check=True)
+    subprocess.run(command, check=True)
 
 if not os.path.exists(venv_dir):
     print("Creating virtual environment...")
@@ -17,9 +17,11 @@ if not os.path.exists(venv_dir):
     print("Virtual environment created.")
 
 if sys.platform == "win32":
-    run_command(f"call {venv_activate} && {PYTHON_EXE} -m pip install -r Calendar/requirements.txt && {PYTHON_EXE} -m pip install python-dateutil")
+    run_command(f"{PYTHON_EXE} -m pip install -r requirements.txt")
+    run_command(f"{PYTHON_EXE} -m pip install python-dateutil")
 else:
-    run_command(f"source {venv_activate} && {PYTHON_EXE} -m pip install -r requirements.txt && {PYTHON_EXE} -m pip install python-dateutil")
+    run_command(f"{PYTHON_EXE} -m pip install -r requirements.txt")
+    run_command(f"{PYTHON_EXE} -m pip install python-dateutil")
 
 print("Running YourBestie/main.py...")
 run_command(f"{PYTHON_EXE} YourBestie/main.py")
